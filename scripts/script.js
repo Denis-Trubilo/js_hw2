5237563d60eba06601e7b708011683c004a28a29
Denis <dtrubilo93@mail.com>
2022-03-29 22:24:34 +0300

    js-hw2

---
+++ scripts/script.js
@@ -0,0 +1,87 @@
+//Задание 1
+
+/*let userName = prompt('Введите ваше имя', '');
+let age = prompt('Введите ваш возраст', 100);
+let city = prompt('Введите ваш город', '');
+let phone = prompt('Введите ваш телефон', '');
+let email = prompt('Введите ваш адрес электронной почты', '');
+let company = prompt('Где вы работаете?', '');
+document.write('Меня зовут ' + userName + '.' + ' Мне ' + age + ' лет' + '.' + ' Я проживаю в городе ' + city + ' и работаю в компании ' + company + '.' + ' Мои контактные данные: ' + phone + ', ' + email + '. ');
+
+//Задание 2
+
+let yearOfBirth = 2022 - age;
+document.write (userName + ' родился в ' + yearOfBirth + ' году.');*/
+
+//Задание 3
+
+/*let a = prompt('Введите шесть любых цифр: ');
+if (Number(a[0]) + Number(a[1]) + Number(a[2]) == Number(a[3]) + Number(a[4]) + Number(a[5])) {
+    console.log('да');
+} else {
+    console.log('нет');
+}*/
+
+
+//Задание 4
+
+/*let a = prompt('Введите переменную: ');
+if (a > 0) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}*/
+
+//Задание 5
+
+/*let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+if (a + b > 1) {
+    console.log((a + b) ** 2)
+}*/
+
+//Задание 6
+
+/*let a = 10;
+let b = 2;
+if (2 < a && a < 11 || 6 <= b && b < 14) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}*/
+
+//Задание 7
+
+/*let n = prompt('Введите переменную: ');
+if (n >= 0 && n <= 14) {
+	console.log('В первую четверть.');
+}
+if (n >= 15 && n <= 30) {
+	console.log('Во вторую четверть.');
+}
+if (n >= 31 && n <= 45) {
+	console.log('В третью четверть.');
+}
+if (n >= 46 && n <= 59) {
+	console.log('В четвертую четверть.');
+}*/
+
+//Задание 8
+
+/*let day = prompt('Введите дату: ');
+if (day >= 1 && day <= 10) {
+	console.log('Первая декада');
+}
+if (day >= 11 && day <= 20) {
+	console.log('Вторая декада');
+}
+if (day >= 21 && day <= 31) {
+	console.log('Третья декада');
+}*/
+
